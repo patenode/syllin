b5d9294e94c38b3e448486f2805f99fcf95126ac
Austin Scott <xeirxes@gmail.com>
2017-02-23 10:02:35 -0800

    Create run.py for initialization of the app

---
+++ run.py
@@ -0,0 +1,8 @@
+from syllin import application
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
--- syllin/__init__.py
+++ syllin/__init__.py
@@ -50,10 +50,3 @@ def initDB():
     db.create_all()
     setupDatabaseForDebug()
     db.session.commit()
-
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
-    application.run()
