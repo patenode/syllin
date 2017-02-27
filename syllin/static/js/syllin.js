2badce487d78db1f3d8a9cacb67e9c3ea8eb845c
Austin Scott <xeirxes@gmail.com>
2017-02-27 00:01:37 -0800

    Clear syllin.js file; itwasn't actually doing what it was there for. Centralize feed variables so that we will be able to add responsiveness without too much trouble.

--- syllin/static/js/syllin.js
+++ syllin/static/js/syllin.js
@@ -2,9 +2,3 @@
  * Created by Austin Scott on 2/26/2017.
  */
 
-jQuery(window).load(function () {
-    var $rowHeight = $('.grid-sizer').width();
-    $('.grid-item').offsetHeight($rowHeight);
-    $('.grid-item--width-2').offheight($rowHeight * 2);
-});
-
